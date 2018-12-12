ee5928e71ba8cf769b47118c8ace8abddc6549ce
RenaldoV <u12204359@gmail.com>
2018-12-12 14:34:47 +0200

    removed unnecessary modules

--- config/DB.js
+++ config/DB.js
@@ -1,4 +1,4 @@
 module.exports = {
-  DB: 'mongodb://test:rootTest1@ds145093.mlab.com:45093/convey_feed'
-  /*DB: 'mongodb://localhost/convey_feed'*/
+  /*DB: 'mongodb://test:rootTest1@ds145093.mlab.com:45093/convey_feed'*/
+  DB: 'mongodb://localhost/convey_feed'
 };
