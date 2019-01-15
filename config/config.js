9e2e41dd9b4237d0913092d42704057f9e2e3a24
Renaldo-Desktop <ronnie@georgetown.co.za>
2019-01-15 09:07:46 +0200

    added test server to config file

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   bccEmail: 'conveyfeed@bassoninc.co.za',
   host: 'http://localhost:4000',
+  /*host: 'http://154.66.198.163:4000',*/
   DB: 'mongodb://test:rootTest1@ds145093.mlab.com:45093/convey_feed'
   /*DB: 'mongodb://localhost/convey_feed'*/
 };
