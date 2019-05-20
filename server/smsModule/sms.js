b138b6793c66e7c56dda000ef64e8c5f384b6a68
RenaldoV <u12204359@gmail.com>
2019-05-20 11:56:12 +0200

    New SMS API Complete. Push and test on server

--- server/smsModule/sms.js
+++ server/smsModule/sms.js
@@ -1,13 +1,139 @@
 let request = require('request');
 let http = require('http');
 const config = require('../../config/config');
+const xml2js = require('xml2js');
+const soapRequest = require('easy-soap-request');
 
 class Sms {
 
-  constructor() {}
-
-  send(contact, message) {
-    const ct = this.parseContact(contact);
+  constructor() {
+    this.headers = {
+      'Content-Type': 'text/xml;charset=UTF-8'
+    };
+  }
+  login() {
+    let loginBody = {api_action: 'login', api_username: config.smsApiUsername, api_password: config.smsApiPassword};
+    let builder = new xml2js.Builder({rootName: 'sms_api'});
+    let xml = builder.buildObject(loginBody);
+    return (async () => {
+      try {
+        const { response } = await soapRequest(config.smsApiUrl, this.headers, xml);
+        let parser = new xml2js.Parser();
+        parser.parseString(response.body, (err, res) => {
+          // parse xml response
+          if(err) {
+            throw new Error(err);
+          } else if(res.sms_api.error_type) {
+            // some error occured
+            throw new Error(res.sms_api.error_type[0]);
+          } else if(res.sms_api.session_id[0]) {
+            response.result = res.sms_api.session_id[0];
+          }
+        });
+        return response.result;
+      } catch (err) {
+        throw err;
+      }
+    })()
+  }
+  logout(sessionid) {
+    let loginBody = {api_action: 'logout', api_sessionid: sessionid};
+    let builder = new xml2js.Builder({rootName: 'sms_api'});
+    let xml = builder.buildObject(loginBody);
+    return (async () => {
+      try {
+        const { response } = await soapRequest(config.smsApiUrl, this.headers, xml);
+        let parser = new xml2js.Parser();
+        parser.parseString(response.body, (err, res) => {
+          response.result = res.sms_api.status[0];
+        });
+        return response.result;
+      } catch (err) {
+        console.log(err.Error);
+      }
+    })()
+  }
+  sendMessage(sessionid, message, contact) {
+    let messageBody = {
+      api_action: 'sendmessages', api_sessionid: sessionid, action_content: {
+        sms : {
+          msisdn: this.parseContact(contact),
+          message: message
+        }
+      }
+    };
+    let builder = new xml2js.Builder({rootName: 'sms_api'});
+    let xml = builder.buildObject(messageBody);
+    return (async () => {
+      try {
+        const { response } = await soapRequest(config.smsApiUrl, this.headers, xml);
+        let parser = new xml2js.Parser();
+        parser.parseString(response.body, (err, res) => {
+          // parse xml response
+          if(err) {
+            throw new Error(err);
+          } else if(res.sms_api.error_type) {
+            // some error occured
+            throw new Error(res.sms_api.error_type[0]);
+          } else if(res.sms_api.sms[0].submit) {
+            if (res.sms_api.sms[0].submit[0].indexOf('Failed') > -1) {
+              // message failed so throw error
+              throw new Error(res.sms_api.sms[0].submit[0]);
+            }
+            response.result = res.sms_api.sms[0].submit[0];
+          }
+        });
+        return response.result;
+      } catch (err) {
+        throw err;
+      }
+    })()
+  }
+  getAccountInfo(sessionid) {
+    let messageBody = {
+      api_action: 'accountinfo', api_sessionid: sessionid};
+    let builder = new xml2js.Builder({rootName: 'sms_api'});
+    let xml = builder.buildObject(messageBody);
+    return (async () => {
+      try {
+        const { response } = await soapRequest(config.smsApiUrl, this.headers, xml);
+        let parser = new xml2js.Parser();
+        parser.parseString(response.body, (err, res) => {
+          // parse xml response
+          if(err) {
+            throw new Error(err);
+          } else if(res.sms_api) {
+            // some error occured
+            response.result = res.sms_api;
+          }
+        });
+        return response.result;
+      } catch (err) {
+        return new Promise((resolve, reject) => {
+          reject(err);
+        })
+      }
+    })()
+  }
+  async send(contact, message) {
+    try {
+      let sessionid = await this.login();
+      let status = await this.sendMessage(sessionid, message, contact);
+      let logout = await this.logout(sessionid);
+      let promise = new Promise((resolve, reject) => {
+        if(status === 'Success') {
+          resolve();
+        } else {
+          reject(status);
+        }
+      });
+      return promise;
+    } catch (e) {
+      if (e) {
+        throw e;
+      }
+    }
+    /*const ct = this.parseContact(contact);
     const msg = encodeURIComponent(encodeURI(message));
     const smsApiUrl = 'http://148.251.196.36/app/smsapi/index.php?key=' + config.smsAPIKey + '&type=text&contacts='+ ct +'&senderid=ULTIMATE&msg=' + msg;
     let promise = new Promise(function(resolve, reject) {
@@ -33,9 +159,8 @@ class Sms {
         });
       }
     });
-    return promise;
+    return promise;*/
   }
-
   parseContact(ct) {
     if (ct[0] === '0' && ct.length === 10) {
       ct = '27'  + ct.substring(1, ct.length);
@@ -45,7 +170,6 @@ class Sms {
     console.log(ct);
     return ct;
   }
-
   commentMade(contact, comment, adminName, propDesc, milestone, footer) {
     const preMessage = adminName + ' added a comment: ' + propDesc + '. ' + milestone + '. ';
     let lengthPreComment = preMessage.length + footer.length + 2; // get length of sms body without comment
@@ -60,9 +184,7 @@ class Sms {
     const message = adminName + ' added a comment: ' + propDesc + '. ' + milestone + '. ' + comment + '. ' + footer;
     // console.log('final total length: ' + message.length);
     return this.send(contact, message)
-      .then(res => {
-        // console.log(res);
-      }).catch(err => {
+      .then(res => {}).catch(err => {
         console.log(err);
       });
   }
@@ -87,8 +209,25 @@ class Sms {
       });
   }
   // Helper function to determine if word starts with a vowel
-  getCredits () {
-    let promise = new Promise(function(resolve, reject) {
+  async getCredits () {
+    try {
+      let sessionid = await this.login();
+      let accountInfo = await this.getAccountInfo(sessionid);
+      let logout = await this.logout(sessionid);
+      let promise = new Promise((resolve, reject) => {
+        if (accountInfo) {
+          resolve(accountInfo.max_total[0]);
+        } else {
+          reject('No data returned');
+        }
+      });
+      return promise;
+    } catch (e) {
+      return new Promise((resolve, reject) => {
+        reject(e);
+      });
+    }
+    /*let promise = new Promise(function(resolve, reject) {
       http.get('http://148.251.196.36/app/miscapi/' + config.smsAPIKey + '/getBalance/true/', (resp) => {
 
         let data = '';
@@ -107,8 +246,8 @@ class Sms {
         reject("Error: " + err.message);
       });
     });
-    return promise;
-  } // TODO: implement get credits
+    return promise;*/
+  }
 }
 
 module.exports = Sms;
